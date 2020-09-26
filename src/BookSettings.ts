2b6f58e489cef9fee88202dac0867636c7393859
Laurens K <39766854+laurensk@users.noreply.github.com>
2020-09-26 16:20:03 +0200

    default values for book size

--- src/BookSettings.ts
+++ src/BookSettings.ts
@@ -9,7 +9,7 @@ export class BookSettings {
   constructor() {
     this.bookId = Number(process.env.BOOK_ID);
     this.bookLastPage = Number(process.env.BOOK_PAGES);
-    this.bookSize = [Number(process.env.BOOK_PDFSIZE_W), Number(process.env.BOOK_PDFSIZE_H)];
+    this.bookSize = [Number(process.env.BOOK_PDFSIZE_W) || 595.28, Number(process.env.BOOK_PDFSIZE_H) || 841.89];
     this.cookies = [
       `ad_session_id=${process.env.COOKIE_SESSION};`,
       `digi4b=${process.env.COOKIE_DIGI4B};`,
