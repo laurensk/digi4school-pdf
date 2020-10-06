e5757b255ec779c6cef016d114d00b0becb7c3f7
Laurens K <39766854+laurensk@users.noreply.github.com>
2020-10-06 11:40:07 +0200

    fixed bookIndex when undefined

--- src/D4SDownloader.ts
+++ src/D4SDownloader.ts
@@ -33,17 +33,28 @@ export class D4SDownlodaer {
   dwlPages(checked: boolean) {
     D4SLog.downloadPage(this.dwlHandler.page);
     let dwlUrl: string;
-    if (this.bookSettings.bookIndex.length != 0 || this.bookSettings.bookIndex != undefined) {
-      dwlUrl =
-        "https://a.digi4school.at/ebook/" +
-        this.bookSettings.bookId +
-        "/" +
-        this.bookSettings.bookIndex +
-        "/" +
-        this.dwlHandler.page +
-        "/" +
-        this.dwlHandler.page +
-        ".svg";
+    if (this.bookSettings.bookIndex) {
+      if (this.bookSettings.bookIndex.length != 0) {
+        dwlUrl =
+          "https://a.digi4school.at/ebook/" +
+          this.bookSettings.bookId +
+          "/" +
+          this.bookSettings.bookIndex +
+          "/" +
+          this.dwlHandler.page +
+          "/" +
+          this.dwlHandler.page +
+          ".svg";
+      } else {
+        dwlUrl =
+          "https://a.digi4school.at/ebook/" +
+          this.bookSettings.bookId +
+          "/" +
+          this.dwlHandler.page +
+          "/" +
+          this.dwlHandler.page +
+          ".svg";
+      }
     } else {
       dwlUrl =
         "https://a.digi4school.at/ebook/" +
