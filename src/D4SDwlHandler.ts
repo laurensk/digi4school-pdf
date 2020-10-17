c0120fe1a88544c390d3dbb40cbbafb9d7fd2e8d
Laurens K <39766854+laurensk@users.noreply.github.com>
2020-10-17 18:23:49 +0200

    persistance storage for props

--- src/D4SDwlHandler.ts
+++ src/D4SDwlHandler.ts
@@ -1,6 +1,14 @@
 import { JSDOM } from "jsdom";
 
 export class D4SDwlHandler {
+  cookies: string = "";
+  bookId: string = "";
+  bookIndex: string = "";
+  bookSize: number[] = [];
+  bookName: string = "";
+
+  isDoneDownloading: boolean = false;
+
   dwlSvgs: JSDOM[] = [];
   page: number = 1;
   pdfMergeNames: string[] = [];
