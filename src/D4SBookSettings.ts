e3025578b175ac87e5a8027fdd519ca7ff282be3
Laurens K <39766854+laurensk@users.noreply.github.com>
2020-10-17 18:23:16 +0200

    removed env booksettings

--- src/D4SBookSettings.ts
+++ src/D4SBookSettings.ts
@@ -1,23 +1,11 @@
-import "./D4SEnv";
-
 export class D4SBookSettings {
-  bookId: number;
-  bookName?: string;
-  bookIndex?: string;
-  bookLastPage: number;
-  bookSize: number[];
-  cookies: string[];
+  bookUrl: string;
+  email: string;
+  password: string;
 
-  constructor() {
-    this.bookId = Number(process.env.BOOK_ID);
-    this.bookIndex = process.env.BOOK_INDEX || null;
-    this.bookName = process.env.BOOK_NAME || null;
-    this.bookLastPage = Number(process.env.BOOK_PAGES);
-    this.bookSize = [Number(process.env.BOOK_PDFSIZE_W) || 595.28, Number(process.env.BOOK_PDFSIZE_H) || 841.89];
-    this.cookies = [
-      `ad_session_id=${process.env.COOKIE_SESSION};`,
-      `digi4b=${process.env.COOKIE_DIGI4B};`,
-      `digi4s=${process.env.COOKIE_DIGI4S};`,
-    ];
+  constructor(bookUrl: string, email: string, password: string) {
+    this.bookUrl = bookUrl;
+    this.email = email;
+    this.password = password;
   }
 }
